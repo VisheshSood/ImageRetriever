e4a7252f2c3ce806b4058d639aca62bd17e78b4c
VISHESH SOOD <omg.itsvishesh@gmail.com>
2015-03-14 05:34:06 -0700

    VERSION3.1
    INDENTATION FIX

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 
 namespace ImageRetriever {
-	
+
 	public partial class Default : System.Web.UI.Page {
-		
+
 
 		protected void UploadBtn_Click(object sender, EventArgs e) {
 			if (FileUpLoad1.HasFile) {
@@ -19,11 +19,11 @@ namespace ImageRetriever {
 				if (extension == ".jpg" || extension == ".jpeg") {
 					if (FileUpLoad1.HasFile) {
 						string filesName = "1" + FileUpLoad1.FileName;
-							Directory.CreateDirectory ("Image/");
-							FileUpLoad1.SaveAs (MapPath ("Image/" + filesName));
+						Directory.CreateDirectory ("Image/");
+						FileUpLoad1.SaveAs (MapPath ("Image/" + filesName));
 					}
 					Response.Redirect (Request.RawUrl);
-				} 
+				}
 			} else {
 				Response.Write("Only .Jpg allowed");
 			}
@@ -45,24 +45,16 @@ namespace ImageRetriever {
 
 		protected void Page_Load(object sender, EventArgs e) {
 			Directory.CreateDirectory ("Image/");
-			if (!IsPostBack)
-			{
+			if (!IsPostBack) {
 				string[] filePaths = Directory.GetFiles(Server.MapPath("~/Image/"));
 				List<ListItem> files = new List<ListItem>();
-				foreach (string filePath in filePaths)
-				
-				{
+				foreach (string filePath in filePaths) {
 					string fileName = Path.GetFileName(filePath);
-
 					files.Add(new ListItem("0,0", "~/Image/" + fileName));
 				}
 				GridView1.DataSource = files;
 				GridView1.DataBind();
 			}
 		}
-	
-			
-
 	}
 }
-
