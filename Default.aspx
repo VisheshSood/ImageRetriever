f741a0cb2f02328fafd13a3923608e29bd04f6b2
VISHESH SOOD <omg.itsvishesh@gmail.com>
2015-03-14 05:28:09 -0700

    Version3.1
    Alignment fixes

--- Default.aspx
+++ Default.aspx
@@ -5,26 +5,29 @@
 	<title>RGB Retriever</title>
 </head>
 <body>
+<center>
 <h1>Image RGB Retriever</h1>
-	<br>Please Enter a File</br>
-	<form id="form1" runat="server">
+	<br> <h3 align="center"> Please Enter A File</h3> </br>
+
+	 <form id="form1" runat="server">
 		<asp:FileUpLoad id="FileUpLoad1" runat="server" />
 		<asp:Button id="UploadBtn" Text="Upload File" OnClick="UploadBtn_Click" runat="server" Width="105px" />	
 		<asp:Button id="DeleteBtn" Text="Delete Folder" OnClick="DeleteBtn_Click" runat="server" Width="105px" />
 		<asp:Button id="DeleteLst" Text="Delete Last" OnClick="DeleteLst_Click" runat="server" Width="105px" />
-	</form>
+	</form> 
 	<hr />
 
 	<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" ShowHeader="false">
 
     <Columns>
         <asp:ImageField DataImageUrlField="Value" ControlStyle-Height=500 ControlStyle-Width=500/>
+    	
         <asp:BoundField DataField="Text" />
 
     </Columns>
   
 	</asp:GridView>
-
+	</center>
 </body>
 </html>
 
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Web.UI.WebControls;
 using System.Linq;
+using System.Windows.Forms;
 
 
 namespace ImageRetriever {
@@ -15,10 +16,11 @@ namespace ImageRetriever {
 		protected void UploadBtn_Click(object sender, EventArgs e) {
 			if (FileUpLoad1.HasFile) {
 				string extension = System.IO.Path.GetExtension(FileUpLoad1.FileName);
-				if (extension == ".jpg" || extension == ".jpeg" || extension == ".png") {
+				if (extension == ".jpg" || extension == ".jpeg") {
 					if (FileUpLoad1.HasFile) {
-						Directory.CreateDirectory ("Image/");
-						FileUpLoad1.SaveAs(MapPath("Image/" + "1" + FileUpLoad1.FileName));
+						string filesName = "1" + FileUpLoad1.FileName;
+							Directory.CreateDirectory ("Image/");
+							FileUpLoad1.SaveAs (MapPath ("Image/" + filesName));
 					}
 					Response.Redirect (Request.RawUrl);
 				} 
@@ -48,19 +50,18 @@ namespace ImageRetriever {
 				string[] filePaths = Directory.GetFiles(Server.MapPath("~/Image/"));
 				List<ListItem> files = new List<ListItem>();
 				foreach (string filePath in filePaths)
+				
 				{
 					string fileName = Path.GetFileName(filePath);
+
 					files.Add(new ListItem("0,0", "~/Image/" + fileName));
 				}
-
 				GridView1.DataSource = files;
 				GridView1.DataBind();
 			}
 		}
-
-		private void pictureClicked(object sender, ImageClickEventArgs e) {
-			Label1.Text = "You clicked the ImageButton control at the coordinates: (" + e.X.ToString() + ", " + e.Y.ToString() + ")";
-		}
+	
+			
 
 	}
 }
