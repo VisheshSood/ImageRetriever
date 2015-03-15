ec689b52db658ed149ad2771b962526e2c6153df
VISHESH SOOD <omg.itsvishesh@gmail.com>
2015-03-15 00:49:31 -0700

    Version4
    Edits with x coordinates

--- Default.aspx
+++ Default.aspx
@@ -1,8 +1,20 @@
-﻿<%@ Page Language="C#" Inherits="ImageRetriever.Default" %>
+﻿<%@ Page Language="C#" Inherits="ImageRetriever.Default" EnableEventValidation ="false"%>
 <!DOCTYPE html>
 <html>
 <head runat="server">
 	<title>RGB Retriever</title>
+
+	<script language="JavaScript">
+	function point_it(event){
+	pos_x = event.offsetX?(event.offsetX):event.pageX-document.getElementById("pointer_div").offsetLeft;
+	pos_y = event.offsetY?(event.offsetY):event.pageY-document.getElementById("pointer_div").offsetTop;
+	document.getElementById("cross").style.left = (pos_x-1) ;
+	document.getElementById("cross").style.top = (pos_y-15) ;
+	document.getElementById("cross").style.visibility = "visible" ;
+	document.pointform.form_x.value = pos_x;
+	document.pointform.form_y.value = pos_y;
+}
+</script>
 </head>
 <body>
 <center>
@@ -17,17 +29,28 @@
 	</form> 
 	<hr />
 
+	<form name="pointform" method="post">
+	<div id="pointer_div" onclick="point_it(event)" style = "background-image:url('~/Image/2.jpg');width:250px;height:250px;repeat">
+	<img src="" id="cross" style="position:relative;visibility:hidden;z-index:2;"></div>
+
+	You pointed on x = <input type="text" name="form_x" size="4" /> - y = <input type="text" name="form_y" size="4" />
+	<br></br>
+	<br></br>
+	<br></br>
+	<br></br>
+	Here are the files you have uploaded.
 	<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" ShowHeader="false">
 
     <Columns>
-        <asp:ImageField DataImageUrlField="Value" ControlStyle-Height=500 ControlStyle-Width=500/>
-    	
-        <asp:BoundField DataField="Text" />
+    <asp:ImageField DataImageUrlField="Value" ControlStyle-Height=500 ControlStyle-Width=500/>
 
+    <asp:BoundField DataField="Text" />
     </Columns>
   
 	</asp:GridView>
-	</center>
+
+
+	</center>	
+
 </body>
 </html>
-
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -6,55 +6,79 @@ using System.Collections.Generic;
 using System.Web.UI.WebControls;
 using System.Linq;
 using System.Windows.Forms;
-
+using System.Drawing;
 
 namespace ImageRetriever {
 
 	public partial class Default : System.Web.UI.Page {
 
 
+			
+
 		protected void UploadBtn_Click(object sender, EventArgs e) {
 			if (FileUpLoad1.HasFile) {
 				string extension = System.IO.Path.GetExtension(FileUpLoad1.FileName);
 				if (extension == ".jpg" || extension == ".jpeg") {
 					if (FileUpLoad1.HasFile) {
-						string filesName = "1" + FileUpLoad1.FileName;
+						string filesName = FileUpLoad1.FileName;
 						Directory.CreateDirectory ("Image/");
 						FileUpLoad1.SaveAs (MapPath ("Image/" + filesName));
+						HttpPostedFile pf = FileUpLoad1.PostedFile;
+						System.Drawing.Image bm = System.Drawing.Image.FromStream(pf.InputStream);
+						bm = ResizeBitmap((Bitmap) bm, 250, 250);
+						bm.Save(Path.Combine("Image/", "2.jpg"));
 					}
 					Response.Redirect (Request.RawUrl);
-				}
+				} 
 			} else {
 				Response.Write("Only .Jpg allowed");
 			}
 		}
 
+		private Bitmap ResizeBitmap(Bitmap b, int nWidth, int nHeight) {
+			Bitmap result = new Bitmap(nWidth, nHeight);
+			using (Graphics g = Graphics.FromImage((System.Drawing.Image)result))
+				g.DrawImage(b, 0, 0, nWidth, nHeight);
+			return result;
+		}
 		protected void DeleteBtn_Click(object sender, EventArgs e) {
 			Array.ForEach(Directory.GetFiles("Image/"), File.Delete);
 			Response.Redirect (Request.RawUrl);
 		}
 
 		protected void DeleteLst_Click(object sender, EventArgs e) {
-			var directory = new DirectoryInfo("Image/");
-			var myFile = (from f in directory.GetFiles()
+			bool isEmpty = !Directory.EnumerateFiles("Image/").Any();
+			if (!isEmpty) {
+				var directory = new DirectoryInfo ("Image/");
+				for (int i = 0; i < 2; i++) {
+					var myFile = (from f in directory.GetFiles ()
 				orderby f.LastWriteTime descending
-				select f).First();
-			myFile.Delete();
-			Response.Redirect (Request.RawUrl);
+					             select f).First ();
+					Response.Write (myFile.ToString ());
+					myFile.Delete ();
+				}
+				Response.Redirect (Request.RawUrl);
+			}
 		}
+			
 
 		protected void Page_Load(object sender, EventArgs e) {
 			Directory.CreateDirectory ("Image/");
-			if (!IsPostBack) {
+			if (!IsPostBack)
+			{
 				string[] filePaths = Directory.GetFiles(Server.MapPath("~/Image/"));
 				List<ListItem> files = new List<ListItem>();
-				foreach (string filePath in filePaths) {
+				foreach (string filePath in filePaths){
 					string fileName = Path.GetFileName(filePath);
-					files.Add(new ListItem("0,0", "~/Image/" + fileName));
+					if (!fileName.Equals("2.jpg")) {
+						files.Add(new ListItem("~/Image/" + fileName, "~/Image/" + fileName));
+					}
 				}
 				GridView1.DataSource = files;
 				GridView1.DataBind();
 			}
 		}
+
+
 	}
 }
